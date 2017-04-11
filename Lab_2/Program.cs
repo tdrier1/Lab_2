98dc81b466fef234b0c45d3b01cb5bb882a4f266
tdrier1 <tdrier1@gmail.com>
2017-04-11 14:14:02 -0400

    added feature detecting even number

--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -13,13 +13,19 @@ namespace Lab_2
             Console.WriteLine("PLease enter an integer between 1 and 100");
             int input = Convert.ToInt32(Console.ReadLine());
 
-            int f = (input % 2);
 
             if ((input % 2) != 0)
             {
                 Console.WriteLine("Odd");
 
             }
+
+            else if((input % 2) == 0 && input >=2 && input <= 25)
+            {
+                Console.WriteLine("Even and less than 25");
+            }
+
+
             Console.ReadKey();
         }
     }
