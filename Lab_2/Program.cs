32305c5ac92e9e6301f4abd0418fc4ff6252f7a9
tdrier1 <tdrier1@gmail.com>
2017-04-11 14:09:04 -0400

    added feature detecting odd numbers

---
+++ Lab_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Automate
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PLease enter an integer between 1 and 100");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            int f = (input % 2);
+
+            if ((input % 2) != 0)
+            {
+                Console.WriteLine("Odd");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
